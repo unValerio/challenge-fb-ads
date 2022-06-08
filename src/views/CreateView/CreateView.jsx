a2d86218571986a2f7ec85d02275db6b94c4f880
Valerio <sergioenrique@me.com>
2022-06-08 23:57:47 +0200

    Creation and view of ads without UX

--- src/views/CreateView/CreateView.jsx
+++ src/views/CreateView/CreateView.jsx
@@ -1,12 +1,49 @@
-import { useParams } from "react-router-dom";
-
+import useCreateView from './useCreateView';
 import './CreateView.css';
 
 function CreateView() {
-  const params = useParams();
+  const { product, onChange, onSubmit } = useCreateView();
 
   return (
-    <h1 className="page-content">CreateView: {params.productId}</h1>
+    <>
+      <h1 className="page-content">New add for {product.productName}</h1>
+      <form onSubmit={onSubmit}>
+        <div>
+          <label htmlFor="headline">Headline</label>
+          <input
+            type="text"
+            name="headline"
+            id="headline"
+            placeholder="Headline"
+            onChange={onChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="description">Description</label>
+          <input
+            type="text"
+            name="description"
+            id="description"
+            placeholder="Description"
+            onChange={onChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="callToAction">Call to action</label>
+          <input
+            type="text"
+            name="callToAction"
+            id="callToAction"
+            placeholder="Call to action"
+            onChange={onChange}
+            required
+          />
+        </div>
+        <button type="submit">Save</button>
+      </form>
+    </>
   );
 }
 
