b705aad44e64aa7d37598f28abf1af0025614f78
Valerio <sergioenrique@me.com>
2022-06-09 11:18:34 +0200

    FB Ad UX completed

--- src/components/FacebookAd/FacebookAd.jsx
+++ src/components/FacebookAd/FacebookAd.jsx
@@ -4,15 +4,30 @@ function FacebookAd({ ad }) {
   return (
     <div className='wrapper'>
       <div className='container'>
-        <div>top</div>
-        <div>{ad.headline}</div>
+        <div className='ad-top'>
+          <div className='col'>
+            <img className='ad-logo' src="https://admockups.com/images/facebook.png" alt="" />
+          </div>
+          <div className='col'>
+            <div className='ad-name'>MaxsSportingGoods.com</div>
+            <div className='ad-sponsored'>Sponsored</div>
+          </div>
+        </div>
+        <div className='ad-headline'>{ad.headline}</div>
         <img className='ad-image' src={ad.image} alt={ad.headline} />
         <div className='ad-content'>
-          <div>MAXSPORTINGGOODS.COM</div>
-          <div>{ad.headline}</div>
-          <div>{ad.description}</div>
+          <div className='col left-col'>
+            <div className='ad-link'>MAXSSPORTINGGOODS.COM</div>
+            <div className='ad-second-headline'>{ad.headline}</div>
+            <div className='ad-description'>{ad.description}</div>
+          </div>
+          <div className="col right-col">
+            <div className="ad-call-to-action">
+              Download
+            </div>
+          </div>
         </div>
-        <div>buttons</div>
+        <div className='fb-actions'></div>
       </div>
     </div>
   );
